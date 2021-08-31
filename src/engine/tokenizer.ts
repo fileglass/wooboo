50714c63358dd0f53c8849514800dea9f88f2efe
Léderer András <andrew@file.glass>
2021-08-31 02:38:41 +0200

    tweak(engine/tokenizer): remove useless iter

--- src/engine/tokenizer.ts
+++ src/engine/tokenizer.ts
@@ -1,8 +1,4 @@
 export default function Tokenize(str: string): string[] {
     const v = str.match(/(?<=\{)([^\s\{\}]*\n?)(?=\})/g)
-    if (v) {
-        return v.map(tok => tok)
-    } else {
-        return []
-    }
+    return v || []
 }
